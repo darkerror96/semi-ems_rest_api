002e7346ff1baf51d3e8a74e2ccb5483cb96a18b
rutpatel <rutpatel@10.155.21.207>
2019-03-27 14:39:32 -0400

    Added log4j2

--- EMSRest/src/main/java/com/collabera/ems/exception/EMSExceptionHandler.java
+++ EMSRest/src/main/java/com/collabera/ems/exception/EMSExceptionHandler.java
@@ -3,6 +3,8 @@
  */
 package com.collabera.ems.exception;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,18 +17,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class EMSExceptionHandler {
 
+	Logger logger = LogManager.getLogger(EMSExceptionHandler.class);
+
 	@ExceptionHandler(EmployeeNotFoundException.class)
 	public ResponseEntity<?> empNFException(EmployeeNotFoundException e) {
+		logger.error(e.getMessage(), e);
 		return ((BodyBuilder) ResponseEntity.notFound()).body(e.getMessage());
 	}
 
 	@ExceptionHandler(ManagerNotFoundException.class)
 	public ResponseEntity<?> manNFException(ManagerNotFoundException e) {
+		logger.error(e.getMessage(), e);
 		return ((BodyBuilder) ResponseEntity.notFound()).body(e.getMessage());
 	}
 
 	@ExceptionHandler(NullPointerException.class)
 	public ResponseEntity<?> nullFoundException(NullPointerException e) {
+		logger.error(e.getMessage(), e);
 		return ResponseEntity.badRequest().body("NULL");
 	}
 }
